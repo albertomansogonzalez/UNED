6dfbaface88a29a855c74b6fd9fdb877338d2e3c
Alberto Manso <albertomanso@outlook.com>
2025-06-25 17:56:38 +0200

    Ada: variables y tipos

---
+++ Ada/01_Basics/02_variables_tipos.adb
@@ -0,0 +1,20 @@
+with Ada.Text_IO;
+
+procedure Main is
+-- region de Declaraciones
+
+   peso : Integer;                 -- variable sin inicializar
+   edad : Integer := 25;           -- variable inicializada
+   id : constant Integer := 1234;  -- constante
+   altura : Float := 1.75;
+   nombre : String := "Ana";
+   activo : Boolean := True;
+   inicial : Character := 'A';
+
+begin
+-- region de expresiones
+
+   peso := 30;              -- asignacion
+   peso := 2 + peso;        -- expresion
+
+end Main;
