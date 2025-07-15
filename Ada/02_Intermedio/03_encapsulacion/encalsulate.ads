3cd47009432c5dcf7270e48a82d2b5038ef29a19
Alberto Manso <albertomanso@outlook.com>
2025-07-15 17:31:21 +0200

    Ada: usar private para encapsular
    Aunque simplemente si no se pone en el .ads ya se encapsula.
    Ponerlo en private sirve que en paquetes heredados, los hijos si lo vean

---
+++ Ada/02_Intermedio/03_encapsulacion/encalsulate.ads
@@ -0,0 +1,9 @@
+package Encapsulate is
+   
+   procedure Hello;
+
+private ------ a partir de aqui, no es visible para unidades externas
+
+   procedure Hello2;
+   
+end Encapsulate;
---
+++ Ada/02_Intermedio/03_encapsulacion/main.adb
@@ -0,0 +1,9 @@
+with Encapsulate;
+
+procedure Main is
+begin
+
+   Encapsulate.Hello;
+   -- Encapsulate.Hello2; -- Hello2 no es visible (esta debajo de "private")
+
+end Main;
